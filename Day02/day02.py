b1c673c86d2620e8c0e7dd592705218cb14037f7
Florence Beck <fbeck@MacBook-Pro-de-Florence.local>
2021-12-02 23:59:52 +0100

    improvements to day02

--- Day02/day02.py
+++ Day02/day02.py
@@ -17,8 +17,33 @@ def read_file_input(filename):
 def parse_line(line):
     return {
         "direction": line.split()[0],
-        "value": line.split()[1]
+        "value": int(line.split()[1])
     }
+
+
+def increase_horizontal(position, value):
+    position["horizontal"] += value
+
+
+def increase_depth(position, value):
+    position["depth"] += value
+
+
+def decrease_depth(position, value):
+    position["depth"] -= value
+
+
+def increase_aim(position, value):
+    position["aim"] += value
+
+
+def decrease_aim(position, value):
+    position["aim"] -= value
+
+
+def increase_horizontal_depth(position, value):
+    position["horizontal"] += value
+    position["depth"] += position["aim"] * value
 
 
 def part_one(my_input):
@@ -26,19 +51,14 @@ def part_one(my_input):
         "horizontal": 0,
         "depth": 0,
     }
-
+    action = {
+        "up": decrease_depth,
+        "down": increase_depth,
+        "forward": increase_horizontal,
+    }
     for line in my_input:
         pline = parse_line(line)
-        value = int(pline["value"])
-        direction = pline["direction"]
-        if direction == 'forward':
-            position["horizontal"] += value
-        elif direction == 'down':
-            position["depth"] += value
-        elif direction =='up':
-            position["depth"] -= value
-        else :
-            return "Something's wrong with the input"
+        action[pline["direction"]](position, pline["value"])
     return position["horizontal"] * position["depth"]
 
 
@@ -48,26 +68,20 @@ def part_two(my_input):
         "depth": 0,
         "aim": 0,
     }
-
+    action = {
+        "up": decrease_aim,
+        "down": increase_aim,
+        "forward": increase_horizontal_depth,
+    }
     for line in my_input:
         pline = parse_line(line)
-        value = int(pline["value"])
-        direction = pline["direction"]
-        if direction == 'forward':
-            position["horizontal"] += value
-            position["depth"] += position["aim"] * value
-        elif direction == 'down':
-            position["aim"] += value
-        elif direction == 'up':
-            position["aim"] -= value
-        else:
-            return "Something's wrong with the input"
+        action[pline["direction"]](position, pline["value"])
     return position["horizontal"] * position["depth"]
 
 
 if __name__ == '__main__':
     print(f'My part one Test Case answer is {part_one(TEST_CASE["input"])}, expecting {TEST_CASE["part_one_result"]} ')
-    print(f'My part one real answer is {part_one(read_file_input("input.txt"))}')
+    print(f'My part one real answer is {part_one(read_file_input("input.txt"))}') #2039912
     print("------------")
     print(f'My part two Test Case answer is {part_two(TEST_CASE["input"])}, expecting {TEST_CASE["part_two_result"]} ')
-    print(f'My part two real answer is {part_two(read_file_input("input.txt"))}')
+    print(f'My part two real answer is {part_two(read_file_input("input.txt"))}') #1942068080
