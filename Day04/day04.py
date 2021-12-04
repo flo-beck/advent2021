31f48a8f1be1182c7274021e80988fffabdc5473
Florence Beck <fbeck@MacBook-Pro-de-Florence.local>
2021-12-04 20:52:33 +0100

    day04 with Gerauds advice

--- Day04/day04.py
+++ Day04/day04.py
@@ -1,4 +1,5 @@
 # https://adventofcode.com/2021/day/4
+from typing import List
 
 TEST_CASE = {
     "input": ["7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1","","22 13 17 11  0", "8  2 23  4 24","21  9 14 16  7", "6 10  3 18  5", "1 12 20 15 19","", "3 15  0  2 22", "9 18 13 17  5","19  8  7 25 23","20 11 10 24  4","14 21 16 12  6","","14 21 17 24  4","10 16 15  9 19","18  8 23 26 20","22 11 13  6  5", "2  0 12  3  7",],
@@ -20,10 +21,9 @@ def make_obj_from_int(int_val):
     }
 
 
-def str_numbers_to_list(line, char):
+def str_numbers_to_list(line, char) -> List[int]:
     my_split = line.split(char)
-    my_list = [int(val) for val in list(filter(None, my_split))]
-
+    my_list = [int(val) for val in my_split]
     return my_list
 
 
@@ -32,12 +32,11 @@ def str_numbers_to_list_obj(line, char):
 
 
 def parse_boards(lines):
-    boards = []
+    boards = [[]]
     x = 0
-    boards.append([])
     for line in lines:
         if line != "":  # a new board
-            numbers_from_str = str_numbers_to_list_obj(line, ' ')
+            numbers_from_str = str_numbers_to_list_obj(line, None)
             boards[x].append(numbers_from_str)
         else:
             x += 1
@@ -64,15 +63,15 @@ def print_boards(boards):
 
 
 def apply_num_to_boards(boards, number_to_find):
-    for x in range(len(boards)):  # each board in boards
-        for y in range(len(boards[x])):  # each row in board
-            for number in range(len(boards[x][y])):  # each number in row
-                if boards[x][y][number]["val"] == number_to_find:
-                    boards[x][y][number]["called"] = True
+    for board in boards:
+        for row in board:
+            for number in row:
+                if number["val"] == number_to_find:
+                    number["called"] = True
 
 
 def get_uncalled(row):
-    return list(filter(lambda num: num["called"] is False, row))
+    return list(filter(lambda num: not num["called"], row))
 
 
 def has_winning_row(board):
@@ -115,6 +114,8 @@ def part_one(my_input):
     called_numbers = str_numbers_to_list(my_input[0], ',')
     my_boards = parse_boards(my_input[2:])
 
+    # print_boards(my_boards)
+
     for number in called_numbers:
         # print_boards(my_boards)
         # print(f"------ new round - number is {number}--------")
@@ -135,6 +136,10 @@ def remove_winning_boards(boards):
             boards.remove(board)
 
 
+def get_ongoing_boards(all_boards):
+    return [board for board in all_boards if not (has_winning_row(board) or has_winning_column(board))]
+
+
 def part_two(my_input):
     called_numbers = str_numbers_to_list(my_input[0], ',')
     my_boards = parse_boards(my_input[2:])
@@ -148,11 +153,10 @@ def part_two(my_input):
                 print_board(my_boards[0])
                 return calc_board(my_boards[0], number)
         else:
-            remove_winning_boards(my_boards)
+            # remove_winning_boards(my_boards)
+            my_boards = get_ongoing_boards(my_boards)
 
     return "There was no final board -- how ?"
-
-
 
 
 if __name__ == '__main__':
