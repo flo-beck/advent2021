50041714a4f7a8b3c445cc479c700f69c160de85
Florence Beck <fbeck@MacBook-Pro.local>
2021-12-06 21:28:32 +0100

    solution to part 2 day 06

--- Day06/day06.py
+++ Day06/day06.py
@@ -1,12 +1,11 @@
 # https://adventofcode.com/2021/day/6
-from typing import List
 
 from advent2021.common.read_input import read_file_lines, str_numbers_to_list
-from collections import namedtuple
+
 TEST_CASE = {
     "input": ["3,4,3,1,2"],
     "part_one_result": 5934,
-    "part_two_result": '?',
+    "part_two_result": 26984457539,
 }
 
 
@@ -31,20 +30,42 @@ def part_one(my_input):
     # get result
     return len(my_lanterns)
 
-#
-# def part_two(my_input):
-#     #  setup input
-#
-#
-#     #  do actions
-#
-#
-#     # get result
+
+def setup_counter(list_lanterns):
+    counter = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    for val in list_lanterns:
+        counter[val] += 1
+    return counter
+
+
+def propagate_fish_with_counter(counter):
+    next_counter = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    for x in range(8, -1, -1):
+        if x == 0:
+            zeros = counter[x]
+            next_counter[8] += zeros
+            next_counter[6] += zeros
+        else:
+            next_counter[x-1] = counter[x]
+    return next_counter
+
+
+def part_two(my_input):
+    #  setup input
+    list_lanterns = str_numbers_to_list(my_input[0], ',')
+    my_counter = setup_counter(list_lanterns)
+
+    #  do actions
+    for _ in range(256):
+        my_counter = propagate_fish_with_counter(my_counter)
+
+    # get result
+    return sum(my_counter)
 
 
 if __name__ == '__main__':
     print(f'My part one Test Case answer is {part_one(TEST_CASE["input"])}, expecting {TEST_CASE["part_one_result"]} ')
-    print(f'My part one real answer is {part_one(read_file_lines("input.txt"))}')  #353079
+    print(f'My part one real answer is {part_one(read_file_lines("input.txt"))}')  # 353079
     print("------------")
-    # print(f'My part two Test Case answer is {part_two(TEST_CASE["input"])}, expecting {TEST_CASE["part_two_result"]} ')
-    # print(f'My part two real answer is {part_two(read_file_lines("input.txt"))}') #
+    print(f'My part two Test Case answer is {part_two(TEST_CASE["input"])}, expecting {TEST_CASE["part_two_result"]} ')
+    print(f'My part two real answer is {part_two(read_file_lines("input.txt"))}')  # 1605400130036
